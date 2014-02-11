5cf1de42700ca421b94f5a2e927bf9a149e88d62
zhijia,.zhang <jiahut@gmail.com>
2014-02-11 16:49:45 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,18 @@ case $1 in
   start)
     echo "starting the server"
     ./node_modules/coffee-script/bin/coffee app.coffee > run.log 2>&1 &
+    echo `ps aux | grep -i node | grep -i app.coffee`
     echo "done."
     ;;
   stop)
-    echo "stop the server"
+    echo "stopping the server"
+    echo `ps aux | grep -i node | grep -i app.coffee`
     kill -9 `ps aux | grep -i node | grep -i app.coffee | awk '{ print $2}'`
     echo "done."
     ;;
-  ?|help|'')
+  *)
     echo "useage: "
     echo "  $0 start"
     echo "  $0 stop"
     ;;
-
 esac
